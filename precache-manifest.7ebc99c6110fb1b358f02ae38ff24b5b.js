d5fb0dfbda1c48918aec36fcdbe8f12223e9a7d3
PaulSklv <paulsklvv@gmail.com>
2019-08-12 13:23:12 +0200

    Updates

---
+++ precache-manifest.7ebc99c6110fb1b358f02ae38ff24b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4dd2356b314a651e7424d4b178fa45e",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "0e2442239e90beaea1a9",
+    "url": "/seasons/static/css/main.77acfaa9.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/seasons/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "0e2442239e90beaea1a9",
+    "url": "/seasons/static/js/main.77d4c554.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
